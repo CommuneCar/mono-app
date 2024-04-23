af4e8a38193d55ad2aba313d17ee27eba98d98ba
Tal Kovler <tal06030@googlemail.com>
2024-04-23 23:00:58 +0300

    added simple drawer for profile

--- apps/ui/src/Pages/home-page/home-page.tsx
+++ apps/ui/src/Pages/home-page/home-page.tsx
@@ -1,48 +1,52 @@
 import React, { MouseEvent, useState } from 'react';
-import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
+import { Box, Drawer, ToggleButton, ToggleButtonGroup } from '@mui/material';
 
-import { Page } from './styles';
 import { communities } from './mock';
 import Map from '../../Components/Map';
+import { BottomDrawer, MainMenuButton, Page } from './styles';
 import { CommunityList } from '../../Components/community-list/community-list';
 
 const HomePage: React.FC = () => {
-  const [fullSCreenRides, setFullScreenRides] = useState(false);
+  const [fullScreenRides, setFullScreenRides] = useState(false);
   const [selectedTab, setSelectedTab] = useState<'communities' | 'rides'>(
     'communities',
   );
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
 
   const ChangeSelectedTab = (
     _: MouseEvent<HTMLElement>,
     newTab: 'communities' | 'rides',
   ) => {
-    setSelectedTab(newTab);
+    if (newTab !== null) setSelectedTab(newTab);
   };
 
   return (
     <Page>
-      {!fullSCreenRides && (
-        <div style={{ height: '65%' }}>
+      <Drawer open={isProfileOpen} onClose={() => setIsProfileOpen(false)}>
+        this will be a profile
+      </Drawer>
+      {!fullScreenRides && (
+        <Box style={{ height: '66%' }}>
           <Map
+            destinations={[]}
             mapStyle={'regular'}
             startPoint={[34.781769, 32.079444]}
-            destinations={[]}
           />
-        </div>
+          <MainMenuButton
+            color="primary"
+            onClick={() => setIsProfileOpen(true)}
+          >
+            <MenuIcon />
+          </MainMenuButton>
+        </Box>
       )}
-      <div
-        style={{
-          height: fullSCreenRides ? '100%' : '35%',
-          overflow: 'auto',
-          border: 'solid 1px grey',
-          borderTopLeftRadius: 10,
-          borderTopRightRadius: 10,
-          transition: 'ease-in-out',
-        }}
+      <BottomDrawer
+        sx={{ height: fullScreenRides ? '100%' : '34%' }}
         onScroll={(event) => {
           const { scrollTop } = event.target as HTMLDivElement;
 
-          if (scrollTop > 150 && !fullSCreenRides) {
+          if (scrollTop > 150 && !fullScreenRides) {
             setFullScreenRides(true);
           }
 
@@ -51,7 +55,7 @@ const HomePage: React.FC = () => {
           }
         }}
       >
-        <div style={{ margin: '2%' }}>
+        <Box style={{ margin: '2%' }}>
           <ToggleButtonGroup
             color="primary"
             value={selectedTab}
@@ -61,12 +65,12 @@ const HomePage: React.FC = () => {
             <ToggleButton value={'communities'}>My Communities</ToggleButton>
             <ToggleButton value={'rides'}>My Rides</ToggleButton>
           </ToggleButtonGroup>
-        </div>
+        </Box>
         {selectedTab === 'communities' && (
           <CommunityList communities={communities} />
         )}
         {selectedTab === 'rides' && <>something will be here :)</>}
-      </div>
+      </BottomDrawer>
     </Page>
   );
 };
--- apps/ui/src/Pages/home-page/styles.ts
+++ apps/ui/src/Pages/home-page/styles.ts
@@ -1,13 +1,29 @@
-import { styled } from '@mui/material';
+import { IconButton, styled } from '@mui/material';
 
 const Page = styled('div')(() => ({
-  height: '100vh',
-  width: '100vw',
-  position: 'absolute',
-  left: 0,
   top: 0,
-  float: 'left',
+  left: 0,
   margin: 0,
+  float: 'left',
+  width: '100vw',
+  height: '100vh',
+  position: 'absolute',
 }));
 
-export { Page };
+const BottomDrawer = styled('div')(() => ({
+  overflow: 'auto',
+  borderTopLeftRadius: 10,
+  borderTopRightRadius: 10,
+  transition: 'ease-in-out',
+  border: 'solid 1px lightgrey',
+}));
+
+const MainMenuButton = styled(IconButton)(() => ({
+  position: 'absolute',
+  top: 10,
+  right: 10,
+  borderRadius: 5,
+  backgroundColor: 'white',
+}));
+
+export { Page, BottomDrawer, MainMenuButton };
