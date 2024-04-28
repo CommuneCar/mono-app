ac9a985b5961409cc98f8bb027e12a24d937bdce
Tal Kovler <tal06030@googlemail.com>
2024-04-28 16:43:29 +0000

    will be used later

--- apps/ui/src/Pages/Map/index.tsx
+++ apps/ui/src/Pages/Map/index.tsx
@@ -2,13 +2,13 @@ import React, { CSSProperties } from 'react';
 import { Map } from '../../Components/Map/Map';
 
 // Define start point and destinations
-const startPoint: [number, number] = [34.781769, 32.079444]; // Example start point
-const destinations = [
-  // Example destinations
-  { longitude: 34.773513, latitude: 32.095531 },
-  { longitude: 34.801461, latitude: 32.113314 },
-  { longitude: 34.751869, latitude: 32.050425 },
-];
+// const startPoint: [number, number] = [34.781769, 32.079444]; // Example start point
+// const destinations = [
+//   // Example destinations
+//   { longitude: 34.773513, latitude: 32.095531 },
+//   { longitude: 34.801461, latitude: 32.113314 },
+//   { longitude: 34.751869, latitude: 32.050425 },
+// ];
 
 const MapPage: React.FC = () => {
   // Explicitly typing the mapContainerStyle object using CSSProperties
