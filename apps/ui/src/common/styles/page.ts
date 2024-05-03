b7342ea0994c64c53ed1640508dbe319f9a947bb
Tal Kovler <tal06030@googlemail.com>
2024-05-04 01:37:15 +0300

    added common styling

---
+++ apps/ui/src/common/styles/page.ts
@@ -0,0 +1,13 @@
+import { Box, styled } from '@mui/material';
+
+const Page = styled(Box)(() => ({
+  top: 0,
+  left: 0,
+  margin: 0,
+  float: 'left',
+  width: '100vw',
+  height: '100vh',
+  position: 'absolute',
+}));
+
+export { Page };
