688911bce819ef80300914b4b44b7488fbfff4d9
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-04 19:56:25 +0300

    menu only in the app with user

--- apps/ui/src/Components/Layout/Layout.tsx
+++ apps/ui/src/Components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Badge, Box, Fab, IconButton } from '@mui/material';
 import { Menu } from './Menu/Menu';
 import { Menu as MenuIcon, Mail as MailIcon } from '@mui/icons-material';
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,34 +10,40 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+
+  const hideButtons =
+    location.pathname === '/' || location.pathname === '/signup';
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <Box
-        sx={{
-          position: 'fixed',
-          top: 8,
-          right: 16,
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '22%',
-          zIndex: 1200,
-        }}
-      >
-        <Fab color="primary" onClick={() => setIsMenuOpen(true)}>
-          <MenuIcon />
-        </Fab>
-        <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
-        <Fab color="primary">
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <MailIcon />
-            </Badge>
-          </IconButton>
-        </Fab>
-      </Box>
+      {!hideButtons && (
+        <Box
+          sx={{
+            position: 'fixed',
+            top: 8,
+            right: 16,
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            height: '22%',
+            zIndex: 1200,
+          }}
+        >
+          <Fab color="primary" onClick={() => setIsMenuOpen(true)}>
+            <MenuIcon />
+          </Fab>
+          <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+          <Fab color="primary">
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+          </Fab>
+        </Box>
+      )}
 
       <Box sx={{ flexGrow: 1, padding: 3 }}>{children}</Box>
     </Box>
