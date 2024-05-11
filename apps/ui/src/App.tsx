1645e0a6ea17f2a446ea855791bcd94d32fcae26
Tal Kovler <tal06030@googlemail.com>
2024-05-11 19:34:42 +0300

    quick fixes

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Auth } from '@supabase/auth-ui-react';
-import { Button, CssBaseline } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { Menu as MenuIcon } from '@mui/icons-material';
 import { Session, createClient } from '@supabase/supabase-js';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -11,25 +10,26 @@ import './App.css';
 
 import { MapPage } from './Pages/Map';
 import RidesFeed from './Pages/RidesFeed';
-import { Menu } from './Components/Menu/Menu';
+import SignUp from './Pages/Signup/SignUp';
+import SignIn from './Pages/SignIn/SignIn';
+import { RoleProvider } from './contexts/role';
+import { ProtectedRoute } from './ProtectedRoute';
 import SearchBar from './Components/Map/SearchBar';
 import { HomePage } from './Pages/HomePage/HomePage';
+import { UserProvider } from './hooks/Users/useUser';
 import MapNavigationPage from './Pages/MapNavigation';
 import { CommunitiesFeed } from './Communities/CommunitiesFeed';
 import {
   useGetAllRides,
   useGetAllCommunities,
 } from './hooks/Communities/useGetAllCommunities';
-import { ProtectedRoute } from './ProtectedRoute';
-import { UserProvider } from './hooks/Users/useUser';
-import { RoleProvider } from './contexts/role';
 
 const supabase = createClient(
   'https://guzwjncnbuiiazedbuis.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1endqbmNuYnVpaWF6ZWRidWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxMTkyNjgsImV4cCI6MjAyOTY5NTI2OH0.24aX1cMX4ilkkUoZs-GE-MxWCoqfaE6rmnwRpJMxs-g',
 );
 
-const App: React.FC = () => {  
+const App: React.FC = () => {
   const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
@@ -52,31 +52,6 @@ const App: React.FC = () => {
 
   return session ? (
     <SessionContextProvider supabaseClient={supabase} initialSession={session}>
-      <CssBaseline />
-      <Router>
-        <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
-
-        <Button onClick={() => setIsMenuOpen(true)}>
-          <MenuIcon />
-        </Button>
-
-        <Routes>
-          <Route path="/rides" element={<RidesFeed rides={rides} />} />
-          <Route
-            path="/communities"
-            element={<CommunitiesFeed communities={communities} />}
-          />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/map" element={<MapPage />} />
-          <Route path="/map/navigation" element={<MapNavigationPage />} />
-          <Route path="/search" element={<SearchBar />} />
-        </Routes>
-      </Router>
-    </SessionContextProvider>
-  ) : (
-    <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-  return (
-    <>
       <UserProvider>
         <RoleProvider>
           <CssBaseline />
@@ -99,7 +74,9 @@ const App: React.FC = () => {
           </Router>
         </RoleProvider>
       </UserProvider>
-    </>
+    </SessionContextProvider>
+  ) : (
+    <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
   );
 };
 
