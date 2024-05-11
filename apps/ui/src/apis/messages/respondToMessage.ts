7fe27dbd43a23f5b78fc3f57f0986058eb175a6a
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-11 14:23:21 +0300

    TODO comment

--- apps/ui/src/apis/messages/respondToMessage.ts
+++ apps/ui/src/apis/messages/respondToMessage.ts
@@ -2,6 +2,7 @@ import { RequsetActions } from '../../types/actions';
 
 const respondToMessage = async (messageId: string, action: RequsetActions) => {
   //TODO: if decline need to post a message to the creator user
+  //TODO: handle if the ride is full already
   return { messageId, action };
 };
 
