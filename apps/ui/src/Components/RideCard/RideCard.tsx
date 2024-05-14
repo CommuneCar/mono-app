6362d124ef2bb7538b8f341df50d6c32b88f25a2
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-14 23:59:21 +0300

    marge fix

--- apps/ui/src/Components/RideCard/RideCard.tsx
+++ apps/ui/src/Components/RideCard/RideCard.tsx
@@ -7,8 +7,6 @@ import {
 } from '@mui/material';
 import React, { useState } from 'react';
 import { Send } from '@mui/icons-material';
-import { useSnackbar } from '../../contexts/SnackbarContext';
-import { TEXT } from '../../themes/default/consts';
 import { JoinRideDialog } from '../JoinRide/JoinRide';
 
 interface RideCardProps {
@@ -17,14 +15,8 @@ interface RideCardProps {
 }
 
 const RideCard: React.FC<RideCardProps> = (props) => {
-  const { showMessage } = useSnackbar();
   const { text, driver } = props;
   const [joinRideDialogOpened, setJoinRideDialogOpened] = useState(false);
-
-  const handleJoinRequest = () => {
-    setJoinRideDialogOpened(true);
-    showMessage(TEXT.alerts.SUCCESSFUL_REQUEST, 'success');
-  };
 
   return (
     <Card variant={'outlined'} sx={{ m: 2, borderRadius: 5 }}>
@@ -42,7 +34,7 @@ const RideCard: React.FC<RideCardProps> = (props) => {
           endIcon={<Send />}
           variant={'contained'}
           size={'small'}
-          onClick={handleJoinRequest}
+          onClick={() => setJoinRideDialogOpened(true)}
         >
           Join Ride
         </Button>
