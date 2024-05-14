a4c84e5d029d31d9f10a5cc4b293b9145feb6913
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-15 00:05:37 +0300

    pr fix

--- apps/ui/src/ProtectedRoute.tsx
+++ apps/ui/src/ProtectedRoute.tsx
@@ -4,8 +4,6 @@ import { useUser } from './hooks/Users/useUser';
 const ProtectedRoute = () => {
   const { user } = useUser();
 
-  return <Outlet />;
-
   return !user ? <Navigate to="/" replace /> : <Outlet />;
 };
 
