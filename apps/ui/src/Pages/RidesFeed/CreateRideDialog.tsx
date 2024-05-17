4dd8fcbc9a2c823bd2e5a9ce92a08f6ca616cb65
Erez Drutin <erezdrutin@Erezs-MBP.lan>
2024-05-17 16:34:14 +0300

    mid work commit

--- apps/ui/src/Pages/RidesFeed/CreateRideDialog.tsx
+++ apps/ui/src/Pages/RidesFeed/CreateRideDialog.tsx
@@ -1,153 +1,167 @@
+import React, { useState, useRef } from 'react';
 import {
   Button,
   Dialog,
-  TextField,
   DialogTitle,
   DialogActions,
   DialogContent,
   DialogContentText,
+  TextField,
+  Box,
+  IconButton,
+  Autocomplete,
+  Switch,
+  FormControlLabel,
 } from '@mui/material';
-import React from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider, DateTimePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import SearchIcon from '@mui/icons-material/Search';
+import dayjs from 'dayjs';
 
-import { Ride } from '@communecar/types';
-
+import { axiosClient } from '../../apis/client';
+import { getRandomOption } from '../../utils';
 import tlv from '../../assets/tlv.png';
 import apple from '../../assets/apple.png';
 import camera from '../../assets/camera.png';
 
-import { getRandomOption } from '../../utils';
-
 const options = [tlv, apple, camera];
 
-interface ICreateRideDialog {
-  rides: Ride[];
-  setOpen: (isOpen: boolean) => void;
-  isOpen: boolean;
-}
+const CreateRideDialog = ({ rides, setOpen, isOpen }) => {
+  const [departureTime, setDepartureTime] = useState(dayjs());
+  const [communityName, setCommunityName] = useState('');
+  const [gasMoney, setGasMoney] = useState('');
+  const [pronounsOnly, setPronounsOnly] = useState(false);
+  const [seats, setSeats] = useState('');
+  const startLocationRef = useRef();
+  const destinationRef = useRef();
 
-const CreateRideDialog = ({ rides, setOpen, isOpen }: ICreateRideDialog) => {
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleSearchLocation = async (query) => {
+    try {
+      const response = await axiosClient.get('/api/v1/external/geocode', { params: { location: query } });
+      return response.data.map(location => ({
+        label: location.displayName || location.name,
+        lat: location.lat,
+        lon: location.lon
+      }));
+    } catch (error) {
+      console.error('Error fetching locations:', error);
+      return [];
+    }
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const png = getRandomOption(options);
+    const startLocation = startLocationRef.current?.value;
+    const destination = destinationRef.current?.value;
+
+    const newRide = {
+      communityName,
+      driver: { name: 'Dar Nachmani', id: 5 },
+      departureTime: departureTime.toDate(),
+      startLocationName: startLocation,
+      destinationName: destination,
+      startLocation: [], // Replace with actual coordinates from Autocomplete
+      destination: [], // Replace with actual coordinates from Autocomplete
+      png,
+      gasMoney: parseFloat(gasMoney),
+      pronouns: pronounsOnly,
+      seats: parseInt(seats, 10)
+    };
+    rides.push(newRide);
+    handleClose();
   };
 
-  const [value, setValue] = React.useState<Dayjs | null>(
-    dayjs('2022-04-17T15:30'),
-  );
+  const handleClose = () => setOpen(false);
 
   return (
-    <React.Fragment>
-      <Dialog
-        open={isOpen}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData as any).entries());
-            const driver = 'Dar Nachmani';
-            const departureTime = value!.toDate();
-            console.log(departureTime);
-
-            const startLocation = formJson.startLocation;
-            const destination = formJson.destination;
-            const communityName = formJson.communityName;
-            const png = getRandomOption<string>(options);
-            rides.push({
-              communityName,
-              driver: {
-                name: driver,
-                id: 5,
-              },
-              departureTime,
-              startLocationName: startLocation,
-              destination,
-              png,
-              destinationName: destination,
-              startLocation,
-              gasMoney: 0,
-              pronouns: false,
-              seats: 4
-            });
-            handleClose();
-          },
-        }}
-      >
-        <DialogTitle>Create ride</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To add a ride, please fill all details here. We will post your ride
-            right after.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="communityName"
-            name="communityName"
-            label="Community Name"
-            type="communityName"
-            fullWidth
-            variant="standard"
-          />
-          {/* <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="departureTime"
-            name="departureTime"
+    <Dialog open={isOpen} onClose={handleClose} fullWidth>
+      <DialogTitle>Create Ride</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Fill in the details to add a new ride.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          required
+          margin="dense"
+          id="communityName"
+          label="Community Name"
+          type="text"
+          fullWidth
+          value={communityName}
+          onChange={(e) => setCommunityName(e.target.value)}
+        />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateTimePicker
             label="Departure Time"
-            type="date"
-            fullWidth
-            variant="standard"
-          /> */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DateTimePicker']}>
-              <DateTimePicker
-                label="Departure time"
-                value={value}
-                onChange={(newValue) => setValue(newValue)}
-              />
-            </DemoContainer>
-          </LocalizationProvider>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="startLocation"
-            name="startLocation"
-            label="Start Location"
-            type="startLocation"
-            fullWidth
-            variant="standard"
+            value={departureTime}
+            onChange={setDepartureTime}
+            renderInput={(params) => <TextField {...params} fullWidth />}
           />
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="destination"
-            name="destination"
-            label="Destination"
-            type="destination"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Create</Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+        </LocalizationProvider>
+        <SearchBar options={[]} handleChangeSearchValue={handleSearchLocation} ref={startLocationRef} placeholder="Start Location" />
+        <SearchBar options={[]} handleChangeSearchValue={handleSearchLocation} ref={destinationRef} placeholder="Destination" />
+        <TextField
+          margin="dense"
+          id="gasMoney"
+          label="Gas Money"
+          type="number"
+          fullWidth
+          value={gasMoney}
+          onChange={(e) => setGasMoney(e.target.value)}
+        />
+        <FormControlLabel
+          control={<Switch checked={pronounsOnly} onChange={(e) => setPronounsOnly(e.target.checked)} />}
+          label="Pronouns Only"
+        />
+        <TextField
+          margin="dense"
+          id="seats"
+          label="Seats"
+          type="number"
+          fullWidth
+          value={seats}
+          onChange={(e) => setSeats(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleSubmit}>Create</Button>
+      </DialogActions>
+    </Dialog>
   );
 };
+
+const SearchBar = React.forwardRef(({ options, handleChangeSearchValue, placeholder }, ref) => {
+  const handleChange = (_event, value) => {
+    handleChangeSearchValue(value);
+  };
+
+  return (
+    <Box sx={{ width: '100%', my: 2 }}>
+      <Autocomplete
+        freeSolo
+        options={options}
+        onChange={handleChange}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder={placeholder}
+            ref={ref}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <IconButton onClick={() => handleChangeSearchValue(ref.current?.value)}>
+                  <SearchIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        )}
+      />
+    </Box>
+  );
+});
 
 export default CreateRideDialog;
