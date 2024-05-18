66110c0501af22875f688bbb78c6ee8ce568b1cf
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-18 11:23:09 +0300

    query fix

--- apps/ui/src/apis/communities/createCommnuity.ts
+++ apps/ui/src/apis/communities/createCommnuity.ts
@@ -1,21 +1,6 @@
 import { Community } from '@communecar/types';
 import { graphqlRequest } from '../graphql';
 
-const CREATE_COMMUNITY_MUTATION = `
-  mutation CreateCommunity($input: CreateCommunityInput!) {
-    createCommunity(input: { community: $input }) {
-      community {
-        id
-        ownerId
-        title
-        description
-        lat
-        long
-      }
-    }
-  }
-`;
-
 interface CreateCommunityResponse {
   createCommunity: {
     community: Community;
@@ -26,21 +11,31 @@ const postNewCommunity = async (
   communityNew: Omit<Community, 'id'>,
   userId: number,
 ): Promise<Community> => {
-  const variables = {
+  const createCommunityQuery = `
+    mutation {
+  createCommunity(
     input: {
       community: {
-        ownerId: userId,
-        title: communityNew.name,
-        description: communityNew.description,
-      },
-    },
-  };
-
+        ownerId: ${userId}, 
+        title: "${communityNew.name}",
+         description: "${communityNew.description}"
+      }
+    }
+  ) {
+    community {
+      id
+      ownerId
+      title
+      description
+      lat
+      long
+    }
+  }
+}
+  `;
   try {
-    const data = await graphqlRequest<CreateCommunityResponse>(
-      CREATE_COMMUNITY_MUTATION,
-      variables,
-    );
+    const data =
+      await graphqlRequest<CreateCommunityResponse>(createCommunityQuery);
     return data.createCommunity.community;
   } catch (error) {
     console.error('Error creating community:', error);
