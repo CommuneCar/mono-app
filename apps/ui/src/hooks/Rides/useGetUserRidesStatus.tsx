d158dac4a7ab9c683dcc54fe51e583a813189208
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-19 22:59:13 +0300

    naming

--- apps/ui/src/hooks/Rides/useGetUserRidesStatus.tsx
+++ apps/ui/src/hooks/Rides/useGetUserRidesStatus.tsx
@@ -2,10 +2,10 @@ import { useQuery } from 'react-query';
 import { UserRidesStatus } from '../../types/ride-user-type';
 import { fetchUserRides } from '../../apis/rides/fetch-user-ride-connections';
 
-const useUserRides = (userId: number) => {
+const useGetUserRidesStatus = (userId: number) => {
   return useQuery<UserRidesStatus, Error>(['userRides', userId], () =>
     fetchUserRides(userId),
   );
 };
 
-export { useUserRides };
+export { useGetUserRidesStatus };
