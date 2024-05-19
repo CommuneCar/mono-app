d933c75397ade1e3c8bde90143b1e4adcf6ab960
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-19 20:05:20 +0300

    type for user ride

--- packages/types/src/Enums.ts
+++ packages/types/src/Enums.ts
@@ -5,6 +5,12 @@ export enum UserStatus {
   MANAGER = 'Manager',
 }
 
+export enum UserRideStatus {
+  PENDING = 'Pending',
+  CANCELLED = 'Cancelled',
+  CONFIRMED = 'Confirmed',
+  REJECTED = 'Rejected',
+}
 export enum Gender {
   FEMALE = 'Female',
   MALE = 'Male',
