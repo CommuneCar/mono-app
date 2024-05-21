299d1e814d95812c831776373c081cbc2f1aeb1c
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-21 22:28:53 +0300

    inbox loader

--- apps/ui/src/Components/PageLoader/PageLoader.tsx
+++ apps/ui/src/Components/PageLoader/PageLoader.tsx
@@ -2,13 +2,19 @@ import { Box, CircularProgress } from '@mui/material';
 
 export interface PageLoaderProps {
   isLoading: boolean;
+  minHeight?: number;
+  paddingTop?: number;
 }
 
-const PageLoader: React.FC<PageLoaderProps> = ({ isLoading }) => {
+const PageLoader: React.FC<PageLoaderProps> = ({
+  isLoading,
+  minHeight = 30,
+  paddingTop = 0,
+}) => {
   return (
     <>
       {isLoading ? (
-        <Box minHeight={'30rem'}>
+        <Box minHeight={`${minHeight}rem`} paddingTop={`${paddingTop}rem`}>
           <CircularProgress size={100} thickness={2} variant="indeterminate" />
         </Box>
       ) : (
