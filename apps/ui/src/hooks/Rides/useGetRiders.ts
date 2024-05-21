c5422db583f45ad4959140cff2d11908f8b42719
Zoeshwa <zoe.shwartz@gmail.com>
2024-05-21 23:05:17 +0300

    other on error toasts

--- apps/ui/src/hooks/Rides/useGetRiders.ts
+++ apps/ui/src/hooks/Rides/useGetRiders.ts
@@ -1,9 +1,20 @@
 import { useQuery } from 'react-query';
 import { Rider } from '@communecar/types';
 import { fetchRidersByRideId } from '../../apis/rides/fetch-riders';
+import { useSnackbar } from '../../contexts/SnackbarContext';
+import { TEXT } from '../../themes/default/consts';
 
 export const useGetRidersByRideId = (rideId: number) => {
-  return useQuery<Rider[], Error>(['ridersByRideId', rideId], () =>
-    fetchRidersByRideId(rideId),
+  const { showMessage } = useSnackbar();
+
+  return useQuery<Rider[], Error>(
+    ['ridersByRideId', rideId],
+    () => fetchRidersByRideId(rideId),
+    {
+      onError(error) {
+        console.error('Error in fetch riders for a ride', error);
+        showMessage(TEXT.alerts.REQUEST_FAILED, 'error');
+      },
+    },
   );
 };
--- apps/ui/src/hooks/Rides/useGetUserRidesStatus.tsx
+++ apps/ui/src/hooks/Rides/useGetUserRidesStatus.tsx
@@ -1,10 +1,21 @@
 import { useQuery } from 'react-query';
 import { UserRidesStatus } from '../../types/ride-user-type';
 import { fetchUserRides } from '../../apis/rides/fetch-user-ride-connections';
+import { useSnackbar } from '../../contexts/SnackbarContext';
+import { TEXT } from '../../themes/default/consts';
 
 const useGetUserRidesStatus = (userId: number) => {
-  return useQuery<UserRidesStatus, Error>(['userRides', userId], () =>
-    fetchUserRides(userId),
+  const { showMessage } = useSnackbar();
+
+  return useQuery<UserRidesStatus, Error>(
+    ['userRides', userId],
+    () => fetchUserRides(userId),
+    {
+      onError(error) {
+        console.error('Error in fetch user ride status', error);
+        showMessage(TEXT.alerts.REQUEST_FAILED, 'error');
+      },
+    },
   );
 };
 
