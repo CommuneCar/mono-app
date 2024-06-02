36e55b76979ec7705b2d941a42136a4e09ee1f34
Zoeshwa <zoe.shwartz@gmail.com>
2024-06-02 22:03:43 +0300

    fetch all communities

--- apps/ui/src/apis/communities/fetchAllCommunities.ts
+++ apps/ui/src/apis/communities/fetchAllCommunities.ts
@@ -1,45 +1,16 @@
 import { Community } from '@communecar/types';
 import { graphqlRequest } from '../graphql';
 import { getFetchAllCommunitiesQuery } from '../utils/communitiesQueries';
-
-interface UserNode {
-  profileImage: string | null;
-}
-
-interface UserCommunityNode {
-  userByUserId: UserNode;
-}
-
-interface CommunityNode {
-  id: number;
-  title: string;
-  description: string;
-  lat?: number;
-  long?: number;
-  userCommunitiesByCommunityId: {
-    nodes: UserCommunityNode[];
-  };
-}
-
-interface CommunitiesData {
-  allCommunities: {
-    nodes: CommunityNode[];
-  };
-}
+import { AllCommunitiesData } from '../types/communitiesResponse';
+import { locationExtraction } from '../utils/handleCommunityResponse';
 
 const fetchAllCommunities = async (): Promise<Community[]> => {
   const query = getFetchAllCommunitiesQuery();
 
-  const data = await graphqlRequest<CommunitiesData>(query);
+  const data = await graphqlRequest<AllCommunitiesData>(query);
 
   return data.allCommunities.nodes.map((node): Community => {
-    const location =
-      node.lat && node.long
-        ? {
-            lat: node.lat,
-            lon: node.long,
-          }
-        : undefined;
+    const location = locationExtraction(node);
 
     const picturesUrl = node.userCommunitiesByCommunityId.nodes
       .map((userCommunity) => userCommunity.userByUserId.profileImage)
