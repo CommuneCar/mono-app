a9b2d95407624609b07ea3620e52d77a504cd649
Zoeshwa <zoe.shwartz@gmail.com>
2024-06-08 12:54:51 +0300

    handle problem with managers

--- apps/ui/src/Communities/CommunityCard/ExpandMoreContent/ExpandMoreContent.tsx
+++ apps/ui/src/Communities/CommunityCard/ExpandMoreContent/ExpandMoreContent.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from '@mui/material';
 import { Location, User } from '@communecar/types';
 import { UserAvatarChips } from '../../../Components/UserAvatar/userAvatarChips';
 import { ExpandMoreFieldBox } from './expandMoreFieldBox.styled';
+import { isEmpty } from 'lodash';
 
 export interface ExpandMoreContentProps {
   communityLocation?: Location;
@@ -22,24 +23,26 @@ const ExpandMoreContent: React.FC<ExpandMoreContentProps> = ({
           {communityLocation?.name ?? 'No Base Location'}
         </Typography>
       </ExpandMoreFieldBox>
-      <ExpandMoreFieldBox>
-        <Typography variant="subtitle2" sx={{ marginBottom: '0.5rem' }}>
-          Managers
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            alignItems: 'stretch',
-            justifyContent: 'flex-start',
-            flexDirection: 'row',
-          }}
-        >
-          {communityOwners.map((owner) => (
-            <UserAvatarChips user={owner} />
-          ))}
-        </Box>
-      </ExpandMoreFieldBox>
+      {!isEmpty(communityOwners) && (
+        <ExpandMoreFieldBox>
+          <Typography variant="subtitle2" sx={{ marginBottom: '0.5rem' }}>
+            Managers
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignItems: 'stretch',
+              justifyContent: 'flex-start',
+              flexDirection: 'row',
+            }}
+          >
+            {communityOwners.map((owner) => (
+              <UserAvatarChips user={owner} />
+            ))}
+          </Box>
+        </ExpandMoreFieldBox>
+      )}
     </>
   );
 };
