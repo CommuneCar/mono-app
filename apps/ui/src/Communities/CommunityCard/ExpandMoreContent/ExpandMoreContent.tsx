806cbfe1e1f0bc81c726b213cb9c720ba6bb97fd
Zoeshwa <zoe.shwartz@gmail.com>
2024-06-08 12:22:04 +0300

    multimangars

--- apps/ui/src/Communities/CommunityCard/ExpandMoreContent/ExpandMoreContent.tsx
+++ apps/ui/src/Communities/CommunityCard/ExpandMoreContent/ExpandMoreContent.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Box, Chip, Tooltip, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Location, User } from '@communecar/types';
+import { UserAvatarChips } from '../../../Components/UserAvatar/userAvatarChips';
 
 export interface ExpandMoreContentProps {
   communityLocation?: Location;
@@ -15,7 +16,7 @@ const ExpandMoreContent: React.FC<ExpandMoreContentProps> = ({
       <Box
         sx={{
           display: 'flex',
-          flexDirection: 'row',
+          flexDirection: 'column',
           justifyContent: 'left',
           alignItems: 'baseline',
         }}
@@ -30,7 +31,7 @@ const ExpandMoreContent: React.FC<ExpandMoreContentProps> = ({
       <Box
         sx={{
           display: 'flex',
-          flexDirection: 'row',
+          flexDirection: 'column',
           justifyContent: 'left',
           alignItems: 'center',
         }}
@@ -38,23 +39,19 @@ const ExpandMoreContent: React.FC<ExpandMoreContentProps> = ({
         <Typography variant="subtitle2" sx={{ marginRight: '0.5rem' }}>
           Managers
         </Typography>
-        {communityOwners.map((owner) => (
-          <Tooltip title={`${owner.firstName} ${owner.lastName}`}>
-            <Chip
-              avatar={
-                <Avatar
-                  alt="Natacha"
-                  src={
-                    owner.avatarUrl ??
-                    'https://guzwjncnbuiiazedbuis.supabase.co/storage/v1/object/public/profile-images/Screenshot%202023-09-08%20at%2020.33.27.png'
-                  }
-                />
-              }
-              label={`${owner.firstName} ${owner.lastName}`}
-              variant="outlined"
-            />
-          </Tooltip>
-        ))}
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'stretch',
+            justifyContent: 'flex-start',
+            flexDirection: 'row',
+          }}
+        >
+          {communityOwners.map((owner) => (
+            <UserAvatarChips user={owner} />
+          ))}
+        </Box>
       </Box>
     </>
   );
---
+++ apps/ui/src/Components/UserAvatar/userAvatarChips.tsx
@@ -0,0 +1,23 @@
+import { User } from '@communecar/types';
+import { Avatar, Chip, Tooltip } from '@mui/material';
+import { getUserFullName } from '../../utils/user/userUtils';
+
+export interface UserAvatarChipsProps {
+  user: User;
+}
+
+const UserAvatarChips: React.FC<UserAvatarChipsProps> = ({ user }) => {
+  const fullName = getUserFullName(user);
+  return (
+    <Tooltip title={fullName}>
+      <Chip
+        sx={{ marginRight: '2px', borderRadius: '16px' }}
+        avatar={<Avatar alt={fullName} src={user.avatarUrl} />}
+        label={fullName}
+        variant="outlined"
+      />
+    </Tooltip>
+  );
+};
+
+export { UserAvatarChips };
