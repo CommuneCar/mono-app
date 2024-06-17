cbcdd9e79ef0dfac14757ecfa26a375d07bca490
Erez Drutin <71937141+erezdrutin@users.noreply.github.com>
2024-06-17 20:09:36 +0300

    Added ride passenger fee price calculation (#195)
    * Added ride passenger fee price calculation
    
    * reverted trip changes from shared packages
    
    * Updated based on PR comments
    
    * Supposedly improved the route calculation method, though it still kinda seem to suck
    
    ---------
    
    Co-authored-by: Erez Drutin <erezdrutin@Erezs-MBP.lan>

--- apps/ui/src/hooks/Rides/useGetTrip.tsx
+++ apps/ui/src/hooks/Rides/useGetTrip.tsx
@@ -4,13 +4,18 @@ import { useSnackbar } from '../../contexts/SnackbarContext';
 import { TEXT } from '../../themes/default/consts';
 import { TripRouteLocation } from '@communetypes/Trip';
 
+interface TripResponse {
+  passengersCount: number;
+  stops: TripRouteLocation[];
+}
+
 const useGetTrip = (rideId: number) => {
   const { showMessage } = useSnackbar();
 
   const queryKey = ['trip', rideId];
   const queryFn = () => fetchTripRoute(rideId);
 
-  return useQuery<TripRouteLocation[], Error>(queryKey, queryFn, {
+  return useQuery<TripResponse, Error>(queryKey, queryFn, {
     onError: (error) => {
       console.error('Error fetching trip:', error);
       showMessage(TEXT.alerts.FETCH_USER_TRIP_FAILED, 'error');
